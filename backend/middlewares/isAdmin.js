40d59878bbe236ebe0a4e32eb763877959faf018
chicowilliam <viniciuswilliam91@gmail.com>
2026-03-29 16:10:16 -0300

    fix(middleware): translate isAdmin error messages to Portuguese

--- backend/middlewares/isAdmin.js
+++ backend/middlewares/isAdmin.js
@@ -2,11 +2,11 @@ const AppError = require("../utils/AppError");
 
 module.exports = function isAdmin(req, res, next) {
   if (!req.session.user) {
-    return next(new AppError("Not authenticated", 401, "NOT_AUTHENTICATED"));
+    return next(new AppError("Não autenticado", 401, "NOT_AUTHENTICATED"));
   }
 
   if (req.session.user.role !== "admin") {
-    return next(new AppError("Access denied", 403, "ACCESS_DENIED"));
+    return next(new AppError("Acesso negado", 403, "ACCESS_DENIED"));
   }
 
   next();
